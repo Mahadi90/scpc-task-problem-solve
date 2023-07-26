a9410aab0a1ecf6cc9308cc5ca6d8717e8b0d6f5
Mahadi90 <mahadimhs787@gmail.com>
2023-07-26 15:41:07 +0600

    task-2 complete

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ function reverseString(inputString){
 }
 
 // console.log(reverseString('Hello World'))
+
+
+// Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
+
+const inputArray = [2, -5, 10, -3, 7];
+
+function sumOfPositiveNumbers(numbers) {
+    let sum = 0;
+    for(let i =0; i < numbers.length; i++){
+        if(numbers[i] > 0){
+            sum = sum + numbers[i]
+        }
+    }
+    return sum;
+  }
+
+// console.log(sumOfPositiveNumbers(inputArray)); 
+
+
