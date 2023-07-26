9b21dfd792acba336d086a0879a4339452d7cbac
Mahadi90 <mahadimhs787@gmail.com>
2023-07-26 15:54:56 +0600

    task-4 complete

--- index.js
+++ index.js
@@ -56,3 +56,28 @@ function findMostFrequentElement(arr) {
 
 
 // console.log(findMostFrequentElement(arr));
+
+
+// Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
+
+const sortedArray = [ 4, 5, 6, 8, 7, 9]
+const target = 10;
+
+function findTwoNumbersWithSum(sortedArray, target){
+    let left = 0;
+    let right = sortedArray.length -1;
+    while(left < right){
+     const sum = sortedArray[left] + sortedArray[right];
+
+     if(sum === target){
+        return [left, right]
+     }else if( sum < target){
+        return left ++;
+     }else{
+        right -- ;
+     }
+    }
+    return null;
+}
+
+// console.log(findTwoNumbersWithSum(sortedArray, target))
