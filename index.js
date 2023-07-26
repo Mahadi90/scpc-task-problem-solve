570ee16a29c1ce961f5265a10f8f12d17c817f02
Mahadi90 <mahadimhs787@gmail.com>
2023-07-26 16:06:43 +0600

    task-6 completed

--- index.js
+++ index.js
@@ -104,3 +104,26 @@ function calculator(num1, operator, num2) {
   }
 
 //   console.log(calculator(5, '/', 0))
+
+
+
+// Task 6: Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
+
+function generateRandomPassword(length) {
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberChars = '0123456789';
+    const specialChars = '!@#$%^&*()-_=+[]{}|;:,.<>?';
+  
+    const allChars = uppercaseChars + lowercaseChars + numberChars + specialChars;
+    let password = '';
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * allChars.length);
+      password += allChars.charAt(randomIndex);
+    }
+  
+    return password;
+  }
+
+//   console.log(generateRandomPassword(10))
